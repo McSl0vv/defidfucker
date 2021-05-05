8cbcbdcbf3165a4baff5d3e2e3e527e316306604
Muhammad Rizqi <ikiwoioi11@gmail.com>
2021-05-05 15:38:38 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+	name='defidfucker',
+	version='1.0',
+	description='Mass notifier for Defacer.ID',
+	url='http://github.com/McSl0vv/defidfucker',
+	author="Mc'Sl0vv",
+	author_email="ikiwoioi11@gmail.com",
+	license="MIT",
+	packages=['defidfucker'],
+	zip_safe=False,
+	install_requires=['cloudscrape', 'argparse']
+     )
